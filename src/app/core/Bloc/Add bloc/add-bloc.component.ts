540ffb0c66662413cd62e10115a6fb519bcef6cc
malek7855 <malek.troudi@esprit.tn>
2023-11-18 22:20:23 +0100

    FIXE ADDBLOC / search

--- src/app/core/Bloc/Add bloc/add-bloc.component.ts
+++ src/app/core/Bloc/Add bloc/add-bloc.component.ts
@@ -1,29 +1,43 @@
 import { Component } from '@angular/core';
-import {Bloc} from "../../../model/Bloc";
-import {BlocService} from "../../../service/BlocService/bloc.service";
-import {ActivatedRoute, Router} from "@angular/router";
-import {NgForm} from "@angular/forms";
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Bloc } from '../../../model/Bloc';
+import { BlocService } from '../../../service/BlocService/bloc.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-add-bloc',
   templateUrl: './add-bloc.component.html',
-  styleUrls: ['./add-bloc.component.css']
+  styleUrls: ['./add-bloc.component.css'],
 })
 export class AddBlocComponent {
-  constructor(private blocService:BlocService,private route:ActivatedRoute,private router:Router) {
-  }
+  form: FormGroup;
   bloc = new Bloc();
-  nomUniversite:String="";
-  addbloc(f:NgForm){
 
-
-    this.blocService.addBloc(this.nomUniversite,this.bloc).subscribe((d)=>{
-      console.log("this is title"+this.nomUniversite)
-      this.bloc=d;
-      console.log(this.bloc);
-    })
-    //this.router.navigate(["/listBloc"])
-
+  constructor(
+    private blocService: BlocService,
+    private route: ActivatedRoute,
+    private router: Router,
+    private fb: FormBuilder
+  ) {
+    this.form = this.fb.group({
+      nomBloc: ['', Validators.required],
+      capaciteBloc: [null, Validators.required],
+      status: ['', Validators.required],
+      description: ['', Validators.required],
+    });
   }
 
+  addbloc() {
+    if (this.form.valid) {
+      this.blocService.addBloc("esprit", this.form.value).subscribe((d) => {
+        console.log('this is title' + this.form.get('nomUniversite')?.value);
+        this.bloc = d;
+        console.log(this.bloc);
+      });
+      this.router.navigate([":universite/bloc"])
+    } else {
+      console.log('form is invalid');
+    }
+    //this.router.navigate(["/listBloc"])
+  }
 }
