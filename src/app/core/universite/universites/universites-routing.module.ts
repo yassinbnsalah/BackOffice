aa0c76bf26007b4673c10961d932f0af878206a0
yassinebnsalah <yacinbnsalh@gmail.com>
2023-12-05 20:44:08 +0100

    BeforeIntegration

--- src/app/core/universite/universites/universites-routing.module.ts
+++ src/app/core/universite/universites/universites-routing.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {UniversiteListeComponent} from "../universite-liste/universite-liste.component";
-import {AcceptedUniversiteComponent} from "../accepted-universite/accepted-universite.component";
-import {UniversiteUpdateComponent} from "../universite-update/universite-update.component";
-import {UniversiteDetailComponent} from "../universite-detail/universite-detail.component";
+import { UniversiteListeComponent } from "../universite-liste/universite-liste.component";
+import { AcceptedUniversiteComponent } from "../accepted-universite/accepted-universite.component";
+import { UniversiteUpdateComponent } from "../universite-update/universite-update.component";
+import { UniversiteDetailComponent } from "../universite-detail/universite-detail.component";
 
 
 const routes: Routes = [
@@ -11,21 +11,21 @@ const routes: Routes = [
     path: "", component: UniversiteListeComponent,
 
   },
-  {path:"information", component: UniversiteDetailComponent },
-{
-  path: "acceptedUniversite", component: AcceptedUniversiteComponent,
+  { path: "information", component: UniversiteDetailComponent },
+  {
+    path: "acceptedUniversite", component: AcceptedUniversiteComponent,
 
-},
+  },
 
-{
-  path:"updateUniversite/:id", component: UniversiteUpdateComponent,
+  {
+    path: "updateUniversite/:id", component: UniversiteUpdateComponent,
 
-},
+  },
 
-{
-  path:":universite", component: UniversiteDetailComponent,
+  {
+    path: ":universite", component: UniversiteDetailComponent,
 
-},
+  },
 
 ];
 
